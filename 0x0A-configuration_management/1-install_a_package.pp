7ba6569948f33251381212b5268bc8e5da10e2f8
dhreyes <2590@holbertonschool.com>
2022-01-04 00:48:49 +0000

    Trying different load order

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # Install puppet-lint version 1.1.0
 package { 'puppet-lint':
-  source   => 'https://rubygems.org'
   ensure   => '2.5.0',
   provider => 'gem',
+  source   => 'https://rubygems.org'
 }
